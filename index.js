ed4b2b6004edaa40dbcbe37be51a5d35591c3b09
suyash20-code <suyashpadwal10@gmail.com>
2024-05-02 18:20:34 +0530

    charger customer - payByCustomer

--- index.js
+++ index.js
@@ -27,59 +27,61 @@ app.post("/pay", async (req, res) => {
     }
   });
   
-  app.post("/cardpay", async (req, res) => {
-    try {
-      // Create a Customer
-      const customer = await stripe.customers.create({
-        source: 'tok_mastercard',
-        email: 'paying.user@example.com',
-      });
+  // app.post("/cardpay", async (req, res) => {
+  //   try {
+  //     // Create a Customer
+  //     const customer = await stripe.customers.create({
+  //       source: 'tok_mastercard',
+  //       email: 'paying.user@example.com',
+  //     });
   
-      // Charge the Customer instead of the card
-      const charge = await stripe.charges.create({
-        amount: 1000,
-        currency: 'usd',
-        customer: customer.id,
-      });
+  //     // Charge the Customer instead of the card
+  //     const charge = await stripe.charges.create({
+  //       amount: 1000,
+  //       currency: 'usd',
+  //       customer: customer.id,
+  //     });
   
-      // Save the customer ID and other info in a database for later use
-      // Example: You can save the customer ID to a variable or database for future reference
-      const customerId = customer.id;
-      console.log("id", customerId);
-      // When it's time to charge the customer again, retrieve the customer ID
-      const charge2 = await stripe.charges.create({
-        amount: 1500, // $15.00 this time
-        currency: 'usd',
-        customer: customerId, // Previously stored and retrieved
-      });
+  //     // Save the customer ID and other info in a database for later use
+  //     // Example: You can save the customer ID to a variable or database for future reference
+  //     const customerId = customer.id;
+  //     console.log("id", customerId);
+  //     // When it's time to charge the customer again, retrieve the customer ID
+  //     const charge2 = await stripe.charges.create({
+  //       amount: 1500, // $15.00 this time
+  //       currency: 'usd',
+  //       customer: customerId, // Previously stored and retrieved
+  //     });
   
-      // Respond with success message
-      res.json({ message: "Customer created and charged successfully", customerId });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  });
-  app.post("/create-customer", async (req, res) => {
-    try {
-      // const { name, email } = req.body;
+  //     // Respond with success message
+  //     res.json({ message: "Customer created and charged successfully", customerId });
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.status(500).json({ message: "Internal server error" });
+  //   }
+  // });
+
   
-      // Create a Customer
-      const customer = await stripe.customers.create({
-        // name,
-        email: 'paying.user@example.com',
-        source: 'tok_mastercard'
-      });
-      console.log("Customer Object:", customer);
-      // Display a console log message with the customer ID
-      console.log("Customer ID:", customer.id);
+  // app.post("/create-customer", async (req, res) => {
+  //   try {
+  //     // const { name, email } = req.body;
   
-      res.json({ message: "Customer created successfully", customerId: customer.id });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  });
+  //     // Create a Customer
+  //     const customer = await stripe.customers.create({
+  //       // name,
+  //       email: 'paying.user@example.com',
+  //       source: 'tok_mastercard'
+  //     });
+  //     console.log("Customer Object:", customer);
+  //     // Display a console log message with the customer ID
+  //     console.log("Customer ID:", customer.id);
+  
+  //     res.json({ message: "Customer created successfully", customerId: customer.id });
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.status(500).json({ message: "Internal server error" });
+  //   }
+  // });
   app.post('/payment-sheet', async (req, res) => {
     const {email = `test${Math.floor(Math.random() * 9999) + 1}@domain.com`} =
       req.body;
@@ -95,6 +97,7 @@ app.post("/pay", async (req, res) => {
       {customer: customer.id},
       {apiVersion: '2024-04-10'},
     );
+    console.log("id",customer);
     const setupIntent = await stripe.setupIntents.create({
      customer: customer.id,
       payment_method_types: ['card']
@@ -104,6 +107,71 @@ app.post("/pay", async (req, res) => {
       ephemeralKey: ephemeralKey.secret,
       customer: customer.id,
     });
+  });
+  app.get("/charge-customer", async (req, res) => {
+    const customerId  = req.query.customerId;
+  console.log("cid", customerId);
+    try {
+      const charge = await stripe.charges.create({
+        amount:1500,
+        currency:'usd',
+        customer: customerId,
+      });
+  
+      res.json({ message: "Customer charged successfully", charge });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+  app.post('/payment-sheets', async (req, res) => {
+    // const {email = `test${Math.floor(Math.random() * 9999) + 1}@domain.com`} =
+    //   req.body;
+  
+    // const stripe = new Stripe(stripeSecretKey, {
+    //   apiVersion: '2024-04-10',
+    //   typescript: true,
+    // });
+  
+    const customer = await stripe.customers.create({email:'test8466@domain.com'});
+  
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      {customer: customer.id},
+      {apiVersion: '2024-04-10'},
+    );
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 5099,
+      currency: 'usd',
+      payment_method_types: ['card', 'link'],
+    });
+    return res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+    });
+  });
+
+  app.post("/payByCustomer", async (req, res) => {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: 1500,
+        currency: "INR",
+        payment_method_types: ["card"],
+        customer:"cus_Q22Uo0BTtOfUlv",
+        payment_method:'pm_1PByQBSJIGArQAxoHyMfEkbv',
+        setup_future_usage: 'on_session',
+        payment_method_options: {
+          card: {
+            request_three_d_secure: 'automatic',
+          },
+        },
+      });
+      const clientSecret = paymentIntent.client_secret;
+      res.json({ message: "Payment initiated", clientSecret });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    }
   });
 app.post("/stripe", async (req, res) => {
   const sig = req.headers["stripe-signature"];
